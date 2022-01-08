96ac7c790c0bcf0bc33af462d7a0f0ca4bc79796
DinithKumudika <dinithwalpitagama@gmail.com>
2022-01-08 15:52:25 +0530

    init

---
+++ WAD/views/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Login</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div class="container">
+            <h1 class="login-text">Login</h1>
+            <div class="sub-container-1">
+                <div class="icon-container">
+                    <i class="fa-solid fa-user"></i>
+                </div>
+                <input type="text" name="username" id="username" class="input-box" placeholder="Username">
+            </div>
+            <div class="sub-container-1">
+                <div class="icon-container">
+                    <i class="fa-solid fa-lock"></i>
+                </div>
+                <input type="password" name="pwd" id="pwd" class="input-box" placeholder="Password">
+            </div>
+            <div class="sub-container-2">
+                <input type="checkbox" name="rememberChkBox" id="rememberChkBox" class="ch-1">
+                <label for="remembeChkBox">Remember me</label>
+                <input type="checkbox" name="show-pwd" id="show-pwd" class="ch-2">
+                <label for="show-pwd">Show Password</label>
+            </div>
+            <input type="submit" value="Log In" class="login-btn">
+            <div class="sub-container-3">
+                <p>Not a member? <a href="signup.php">Sign Up</a></p>
+            </div>
+        </div>
+    </form>
+    <script src="../js/login.js"></script>
+</body>
+</html>
---
+++ WAD/views/signup.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../css/signup.css">
+    <title>Signup</title>
+</head>
+<body>
+    <h1 class="heading">Join with us shape you future</h1>
+    <form action="" method="post">
+        <div class="signup-container">
+            <div class="signup-img">
+                <img src="../assets/images/signup.png" alt="signup">
+            </div>
+            <div class="form-container">
+                <h1 class="login-text">SignUp</h1>
+                <label for="f-name">First Name:</label>
+                <input type="text" name="f-name" id="f-name" class="input-box">
+                <label for="l-name">Lirst Name:</label>
+                <input type="text" name="l-name" id="l-name" class="input-box">
+                <label for="l-name">E-mail:</label>
+                <input type="email" name="e-mail" id="e-mail" class="input-box">
+                <div class="sub-container-1">
+                    <label for="l-name">Gender:</label>
+                    <label for="l-name">Male</label><input type="checkbox" name="gender" id="male" class="gender-chkbox">
+                    <label for="l-name">Female</label><input type="checkbox" name="gender" id="female" class="gender-chkbox">
+                </div>
+                <label for="create-pwd">Create Password:</label>
+                <input type="password" name="create-pwd" id="create-pwd" class="input-box">
+                <label for="confirm-pwd">Confirm Password:</label>
+                <input type="password" name="confirm-pwd" id="confirm-pwd" class="input-box">
+                <input type="submit" value="Sign Up" class="login-btn">
+                <div class="sub-container-3">
+                    <p>Already a member? <a href="login.php">Log In</a></p>
+                </div>
+            </div>
+        </div>
+    </form>
+    <script>
+        
+    </script>
+</body>
+</html>
