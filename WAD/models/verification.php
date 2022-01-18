a6173310853b70301e9c860977fa0f1e4ad0ef0b
DinithKumudika <dinithwalpitagama@gmail.com>
2022-01-18 20:15:33 +0530

    zsd

--- WAD/models/verification.php
+++
@@ -1,6 +0,0 @@
-<?php
-  include './db/db_connect.php';
-  if(isset($_GET['verification_code'])){
-    $verification_code = mysqli_real_escape_string($conn,$_GET['verification_code']);
-  }
-?>
--- WAD/views/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>All right reserved by Hireme.lk | 2021</p>
-</footer>
